51745da8b212b4e689783cc5039f3ce41a67295f
Nicolas Knoebber <knoebber@gmail.com>
2019-10-28 23:31:47 -0500

    Add Stripe Elements

--- shop.js
+++ shop.js
@@ -1,5 +1,6 @@
 const APIGateway = 'https://p4b43mv7al.execute-api.us-west-2.amazonaws.com/dev'
 const productID = 'prod_G3nbhaoJkINZ5v';
+const stripePublicKey = 'pk_test_qUdrpKjmC5gZ7jcuuHeRb8Au006WnfLwAt';
 
 function fetchProduct() {
   return fetch(`${APIGateway}/products/${productID}`)
@@ -61,12 +62,15 @@ function buildProductGrid([product, { offers, coupons }]) {
   grid.innerHTML = '';
 
   offers.forEach(({ quantity, couponID }) => {
-    const title = document.createElement('strong');
+    const title = document.createElement('label');
     const price = document.createElement('em');
     const input = document.createElement('input');
 
     if (quantity == 1) title.innerHTML = `${quantity} ${productName}`;
     else title.innerHTML = `${quantity} ${productName}s`;
+
+    const target = `input-quantity-${quantity}`;
+    title.setAttribute('for', target)
 
     // Stripe stores amounts as cent values.
     const grossAmount = productPrice * quantity;
@@ -75,6 +79,7 @@ function buildProductGrid([product, { offers, coupons }]) {
 
     input.setAttribute('type', 'radio');
     input.setAttribute('name', 'product-select');
+    input.setAttribute('id', target);
     input.setAttribute('value', quantity);
 
     // Fade the elements in as they are added to the DOM.
@@ -85,28 +90,71 @@ function buildProductGrid([product, { offers, coupons }]) {
   });
 }
 
-// Fetch the information for the product selection.
-Promise.all([
-  `${APIGateway}/products/${productID}`,
-  `${APIGateway}/coupons`,
-].map((url) => fetch(url)
-  .then((response) => responseHandler(response, url))
-  .catch((err) => console.log(err)))).then(buildProductGrid);
+function placeOrder(token) {
+  const address = [
+    'name',
+    'address',
+    'city',
+    'zip',
+    'state',
+  ].reduce((result, curr) => ({
+    [curr]: document.getElementById(curr).value,
+    ...result
+  }), {});
 
-  /*
-     <strong>1 Can</strong>
-     <em> ...loading prices ... </em>
-     <input type="radio" name="product-select" value="single">
+  console.log(address);
 
-     <strong>3 Cans</strong>
-     <em> ...loading prices ... </em>
-     <input type="radio" name="product-select" value="three">
+  console.log(token);
+}
 
-     <strong>12 Cans</strong>
-     <em> ...loading prices ... </em>
-     <input type="radio" name="product-select" value="half">
+function loadShop() {
+  // Fetch the information for the product selection.
+  Promise.all([
+    `${APIGateway}/products/${productID}`,
+    `${APIGateway}/coupons`,
+  ].map((url) => fetch(url)
+    .then((response) => responseHandler(response, url))
+    .catch((err) => console.log(err)))).then(buildProductGrid);
 
-     <strong>24 Cans</strong>
-     <em> ...loading prices ... </em>
-     <input type="radio" name="product-select" value="full">
-   */
+  const form = document.getElementById('shop-form');
+  const submitButton = document.getElementById('place-order');
+  const formErrors = document.getElementById('form-errors');
+
+  // Initialize Stripe.
+  const stripe = Stripe(stripePublicKey);
+  const elements = stripe.elements();
+
+  const style = {
+    base: {
+      fontSize: '20px',
+      color: '#32325d',
+    },
+  };
+
+  const card = elements.create('card', { style });
+  card.mount('#card-element');
+
+  card.addEventListener('change', ({ error }) => {
+    if (error) {
+      submitButton.setAttribute('disabled', true);
+      formErrors.textContent = error.message;
+    } else {
+      submitButton.setAttribute('disabled', false);
+      formErrors.textContent = '';
+    }
+  });
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const { token, error } = await stripe.createToken(card);
+
+    if (error) {
+      formErrors.textContent = error.message;
+    } else {
+      placeOrder(token);
+    }
+  });
+}
+
+window.onload = loadShop;
