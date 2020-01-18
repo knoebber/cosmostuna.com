aa693757cef210c4132ec03ff89fa0ad7a3b3cf0
Nicolas Knoebber <knoebber@gmail.com>
2020-01-18 12:19:40 -0600

    Simplify terms

--- templates/terms.php
+++ templates/terms.php
@@ -3,19 +3,18 @@
   <h4>Privacy</h4>
   <p>
     Cosmo's Tuna does not gather any customer data outside of orders.
-    We will only use your email to send confirmations when an order is paid, shipped, or canceled.
+    We will use your email only to send confirmations regarding your order.
   </p>
   <p>
-    www.cosmostuna.com uses SSL encryption to protect your payment information when it is sent.
+    Cosmo's Tuna uses SSL encryption to protect your payment information.
     We cannot see or store your payment information; instead it is sent to <a target="_blank" href="https://www.stripe.com">Stripe</a>, a reputable payment processor.
   </p>
   <p>
-    While we do not store any user browsing patterns, we do embed Stripe's fraud protection script on every page, which monitors user activity.
+    While we do not store any user browsing patterns, we do embed Stripe's fraud protection script on every page.
   </p>
   <h4>Terms and Conditions</h4>
   <p>
-    In order protect against fraud, Cosmo's Tuna reserves the right to pick an order and email the address provided before the order is mailed.
-    A customer may contact us after completing payment on an order and before the order is shipped to request a refund.
+    In order to protect against fraud, Cosmo's Tuna reserves the right to pick an order and email the address provided before the order is mailed.
   </p>
   <p>
     All sales are final after shipment.
